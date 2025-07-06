687740db412982538b15500bb599a5f30cec496f
sassanghz <sassanghazi@gmail.com>
2025-07-06 16:38:41 -0400

    importing libs

---
+++ main.py
@@ -0,0 +1,8 @@
+#visualizing the dataset
+import numpy as np
+import matplotlob.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
+
